e98ff1bf804d076a84bce9fd55264ba242537c2c
nuzzyjr <89519055+nuzzyjr@users.noreply.github.com>
2022-02-08 22:10:26 +0000

    WD

---
+++ quiz_layout.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quiz</title>
+    <!-- Bootstrap Morph -->
+    <link href="bootstrap-5.0.2-dist/css/bootstrap.css" rel="stylesheet" />
+    <link href="bootstrap-5.0.2-dist/css/bootstrap-theme.css" rel="stylesheet" />
+</head>
+<body>
+
+    <div class="container">
+        <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+        paAWDPP
+        </div>
+        <div class="col-md-6"></div>
+        </div>
+    </div>
+    
+</body>
+</html>
